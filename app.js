ad1a4cca3faf3b1249286a1bfa9ef016a0c6963a
Kossy-Augusta <kossyudogu@gmail.com>
2024-11-19 12:26:45 +0100

    added event listener to buttons

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 import getELement from "./utils/get-element.js";
+import {pressNumber} from "./utils/dedicated-functions.js";
 
 const display = getELement(".display");
 const btns = [...document.querySelectorAll(".button")];
 
+btns.forEach((btn)=>{
+ btn.addEventListener('click', (e)=>{
+  const type = e.target.dataset.type;
+  const value = e.target.textContent;
+  if(type === "number"){   
+   pressNumber(value)
+  }
+ })
+})
